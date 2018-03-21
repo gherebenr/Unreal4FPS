8606cee74cbee63c0401fad42499fa02d892a047
Unknown <gherebenr@gmail.com>
2018-03-20 17:58:03 -0700

    AI patrolling works in c++

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,9 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" //TODO Remove coupling.
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	UE_LOG(LogTemp, Warning, TEXT("AI C++!"));
+	//TODO Protect against empty patrol routes.
+	
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	//auto Waypoint = BlackboardComp->GetValueAsObject(WaypointKey.SelectedKeyName);
+
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, ((Index + 1) % PatrolPoints.Num()));
+
+
+
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
 
