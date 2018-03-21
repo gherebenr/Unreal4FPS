43cfedb3a79b49b04d36f01e1b506bbbcfad461d
Unknown <gherebenr@gmail.com>
2018-03-21 14:31:03 -0700

    Created component for patrolling.

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,26 +3,29 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" //TODO Remove coupling.
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	//TODO Protect against empty patrol routes.
-	
+	// Get Patrol Route
+	auto Pawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = Pawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() < 1){
+		auto PawnName = Pawn->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("No patrol points for %s"), *PawnName);
+		return EBTNodeResult::Failed;
+	}
+
+	// Get Blackboard Data
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	//auto Waypoint = BlackboardComp->GetValueAsObject(WaypointKey.SelectedKeyName);
 
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
-
+	// Set Blackboard Data
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
 	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, ((Index + 1) % PatrolPoints.Num()));
 
-
-
-	//UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
 	return EBTNodeResult::Succeeded;
 }
 
